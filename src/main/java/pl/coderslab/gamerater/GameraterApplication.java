acf1f1de9ee4ff18efa55b84cdb7349e002baf10
Maximilian Sydry <maximilian.sydry@gmail.com>
2019-01-12 20:46:06 +0100

    New draft

---
+++ src/main/java/pl/coderslab/gamerater/GameraterApplication.java
@@ -0,0 +1,14 @@
+package pl.coderslab.gamerater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameraterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameraterApplication.class, args);
+    }
+
+}
+
